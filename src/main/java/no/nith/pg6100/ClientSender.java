0c3380422a5d92bb3cd2acadf75e61cf2f5cecf4
Naimdjon Takhirov <naimdjon@gmail.com>
2014-02-11 23:19:51 +0100

    LF

--- src/main/java/no/nith/pg6100/ClientSender.java
+++ src/main/java/no/nith/pg6100/ClientSender.java
@@ -1,10 +1,33 @@
 package no.nith.pg6100;
 
+import javax.jms.*;
+import javax.naming.InitialContext;
+
 public class ClientSender {
 
     public static void main(String[] args) {
-        // Fra forrige øving vet dere hvordan en meldingssender skal være
-        // implementer her en sender som sender melding til destinasjon hvor MDB-en lytter på server siden.
-        // Denne skal kjøre på klientsiden (fjernt)
+        try {
+            InitialContext context = new InitialContext();
+            QueueConnectionFactory connectionFactory = (QueueConnectionFactory) context.lookup("jms/QueueConnectionFactory");
+            System.out.println("got queue connection factory: " + connectionFactory.getClass().getName());
+            QueueConnection con = connectionFactory.createQueueConnection();
+            try {
+                Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                try {
+                    Destination destination = (Destination) context.lookup("jms/Queue");
+                    MessageProducer producer = session.createProducer(destination);
+                    TextMessage textMessage = session.createTextMessage();
+                    textMessage.setText("Heia fisk frosk, melding fra basic queue sender!");
+                    producer.send(textMessage);
+                    System.out.println("message sent!");
+                } finally {
+                    session.close();
+                }
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/no/nith/pg6100/MessageReceiverMDB.java
+++ src/main/java/no/nith/pg6100/MessageReceiverMDB.java
@@ -1,8 +1,23 @@
 package no.nith.pg6100;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 
-public class MessageReceiverMDB {
+@MessageDriven(mappedName="jms/Queue", activationConfig =  {
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
+})
+public class MessageReceiverMDB implements MessageListener {
 
-    // Denne klassen skal være vår message driven bean.
+    @PostConstruct
+    public void intitialize(){
+        System.out.println(getClass().getName()+" has been successfully initialized!");
+    }
+
+    public void onMessage(Message message) {
+        System.out.println("Message received!"+message);
+    }
 
 }
